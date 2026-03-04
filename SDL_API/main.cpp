3322791800f59dd6509fda6f5515227f879ab698
Lee JungSun <80955757+jungsuns2@users.noreply.github.com>
2026-03-04 18:34:41 +0900

    bmp 출력(윈도우 객체에서 직접)

--- SDL_API/main.cpp
+++ SDL_API/main.cpp
@@ -18,22 +18,17 @@ int main(int argc, char* argv[])
 	SDL_Window* pWindow = SDL_CreateWindow("bmp", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
 		WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
 
-	// 렌더러 생성
-	SDL_Renderer* pRenderer = SDL_CreateRenderer(pWindow, -1, 0);
+	// 렌더러 대신 윈도우 객체의 SDL_Surface 객체를 얻는다.
+	SDL_Surface* pScreenSurface = SDL_GetWindowSurface(pWindow);
 
 	// bmp 이미지 생성
 	SDL_Surface* pHellowBMP = SDL_LoadBMP("hello.bmp");
 	assert(pHellowBMP != nullptr);
 
-	// 텍스처 객체로 변환
-	SDL_Texture* pTexture = SDL_CreateTextureFromSurface(pRenderer, pHellowBMP);
-	assert(pTexture != nullptr);
-	SDL_FreeSurface(pHellowBMP);
-
+	// 이벤트 처리
 	bool running = true;
 	SDL_Event event;
 
-	// 이벤트 처리
 	while (running)
 	{
 		while (SDL_PollEvent(&event))
@@ -51,15 +46,18 @@ int main(int argc, char* argv[])
 			}
 		}
 
-		SDL_RenderClear(pRenderer);
-		SDL_RenderCopy(pRenderer, pTexture, nullptr, nullptr);
-		SDL_RenderPresent(pRenderer);
+		//  직접 로드한 BMP 이미지를 윈도우 객체로 부터 획득한 SDL_Surface 객체에 복사
+		SDL_BlitSurface(pHellowBMP, nullptr, pScreenSurface, nullptr);
+		
+		// 최종적으로 반영
+		SDL_UpdateWindowSurface(pWindow);
 	}
 
-	SDL_DestroyTexture(pTexture);
-	SDL_DestroyRenderer(pRenderer);
+	SDL_FreeSurface(pHellowBMP);
 	SDL_DestroyWindow(pWindow);
 	SDL_Quit();
 
+	// 결론: GPU 메모리같은 하드웨어 방식을 활용한 방식이 아니라 느릴 수 있다.
+	// 하드웨어 처리를 지원하지 않는 임베디드 장치에서는 좋다.
 	return 0;
 }
