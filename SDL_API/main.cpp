e2cb384aefacc888a41a4dbdbeffb4e540689de7
Lee JungSun <80955757+jungsuns2@users.noreply.github.com>
2026-03-04 19:24:50 +0900

    wav 재생

--- SDL_API/main.cpp
+++ SDL_API/main.cpp
@@ -1,63 +1,45 @@
 #include <iostream>
 #include <SDL.h>
-#include <assert.h>
-
-constexpr int WIDTH = 640;
-constexpr int HEIGHT = 480;
 
 int main(int argc, char* argv[])
 {
-	// SDL 초기화
-	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+	SDL_Init(SDL_INIT_AUDIO);
+	SDL_AudioSpec wav_spec{};
+
+	Uint8* wav_buffer = nullptr;
+	Uint32 wav_length{};
+
+	// 웨이브 파일 로드
+	if (SDL_LoadWAV("example.wav", &wav_spec, &wav_buffer, &wav_length) == NULL)
 	{
-		std::cout << SDL_GetError() << std::endl;
-		return 0;
+		fprintf(stderr, "Failed to load WAV file: %s\n", SDL_GetError());
+		exit(1);
 	}
 
-	// 윈도우 생성
-	SDL_Window* pWindow = SDL_CreateWindow("bmp", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-		WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
-
-	// 렌더러 대신 윈도우 객체의 SDL_Surface 객체를 얻는다.
-	SDL_Surface* pScreenSurface = SDL_GetWindowSurface(pWindow);
-
-	// bmp 이미지 생성
-	SDL_Surface* pHellowBMP = SDL_LoadBMP("hello.bmp");
-	assert(pHellowBMP != nullptr);
-
-	// 이벤트 처리
-	bool running = true;
-	SDL_Event event;
-
-	while (running)
+	// 오디오 장치 열기
+	SDL_AudioDeviceID dev = SDL_OpenAudioDevice(NULL, 0, &wav_spec, NULL, 0);
+	if (dev == 0) 
 	{
-		while (SDL_PollEvent(&event))
-		{
-			if (event.type == SDL_KEYDOWN)
-			{
-				if (event.key.keysym.sym == SDLK_ESCAPE)
-				{
-					running = false;
-				}
-			}
-			else if (event.type == SDL_QUIT)
-			{
-				running = false;
-			}
-		}
-
-		//  직접 로드한 BMP 이미지를 윈도우 객체로 부터 획득한 SDL_Surface 객체에 복사
-		SDL_BlitSurface(pHellowBMP, nullptr, pScreenSurface, nullptr);
-		
-		// 최종적으로 반영
-		SDL_UpdateWindowSurface(pWindow);
+		fprintf(stderr, "Failed to open audio device: %s\n", SDL_GetError());
+		exit(1);
 	}
 
-	SDL_FreeSurface(pHellowBMP);
-	SDL_DestroyWindow(pWindow);
+	// 파일을 재생하기 위해 큐에 넣는다.
+	int success = SDL_QueueAudio(dev, wav_buffer, wav_length);
+	if (success != 0) 
+	{
+		fprintf(stderr, "Failed to queue audio: %s\n", SDL_GetError());
+		exit(1);
+	}
+
+	// 연주하거나 멈춘다.
+	SDL_PauseAudioDevice(dev, 0);
+
+	SDL_Delay(5000);
+
+	SDL_CloseAudioDevice(dev);
+	SDL_FreeWAV(wav_buffer);
 	SDL_Quit();
 
-	// 결론: GPU 메모리같은 하드웨어 방식을 활용한 방식이 아니라 느릴 수 있다.
-	// 하드웨어 처리를 지원하지 않는 임베디드 장치에서는 좋다.
 	return 0;
 }
