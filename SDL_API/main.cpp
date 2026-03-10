62b3f530848f7d9c3fdc2ef50820c729614f9362
Lee JungSun <dlwjdtjs99@naver.com>
2026-03-10 13:17:35 +0900

    간단 마우스 입력 구현

--- SDL_API/main.cpp
+++ SDL_API/main.cpp
@@ -2,20 +2,40 @@
 #include <SDL.h>
 #include <array>
 
+struct Point_2S
+{
+	int32_t x;
+	int32_t y;
+};
+
 bool GetKey(const SDL_Scancode virtualCode);
 bool GetKeyDown(const SDL_Scancode virtualCode);
 bool GetKeyUp(const SDL_Scancode virtualCode);
 void SetKeyState(const SDL_Scancode virtualCode, const bool bPressed);
 
+bool GetMouseButton(const Uint8 virtualMouse);
+bool GetMouseButtonDown(const Uint8 virtualMouse);
+bool GetMouseButtonUp(const Uint8 virtualMouse);
+void SetMouseButtonState(const Uint8 virtualMouse, const bool bPressed);
+
+Point_2S GetMousePosition();
+void SetMousePosition(Point_2S mousePosition);
+
+constexpr int WIDTH = 640;
+constexpr int HEIGHT = 480;
+
 static std::array<bool, SDL_NUM_SCANCODES> KeysPressed;
 static std::array<bool, SDL_NUM_SCANCODES> PrevKeysPressed;
+
+constexpr size_t SDL_MOUSE_COUNT = 6;
+static std::array<bool, SDL_MOUSE_COUNT> MousePressed;
+static std::array<bool, SDL_MOUSE_COUNT> PreMousePressed;
+
+Point_2S MousePosition;
 
 int main(int argc, char* argv[])
 {
 	SDL_Init(SDL_INIT_EVERYTHING);
-
-	constexpr int WIDTH = 640;
-	constexpr int HEIGHT = 480;
 
 	SDL_Window* window = SDL_CreateWindow("Keyboard", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
 		WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
@@ -34,6 +54,15 @@ int main(int argc, char* argv[])
 				case SDL_KEYDOWN:
 				case SDL_KEYUP:
 					SetKeyState(event.key.keysym.scancode, bool(event.type == SDL_KEYDOWN));
+					break;
+
+				case SDL_MOUSEBUTTONDOWN:
+				case SDL_MOUSEBUTTONUP:
+					SetMouseButtonState(event.button.button, bool(event.type == SDL_MOUSEBUTTONDOWN));
+					break;
+
+				case SDL_MOUSEMOTION:
+					SetMousePosition({ event.motion.x, event.motion.y });
 					break;
 
 				case SDL_QUIT:
@@ -73,6 +102,18 @@ int main(int argc, char* argv[])
 			quit = true;
 		}
 
+		if (GetMouseButtonDown(SDL_BUTTON_LEFT))
+		{
+			printf("Left 클릭\n");
+		}
+		if (GetMouseButtonDown(SDL_BUTTON_RIGHT))
+		{
+			printf("Right 클릭\n");
+		}
+
+		printf("%d, %d\n", GetMousePosition().x, GetMousePosition().y);
+
+
 		// 게임 업데이트
 
 		// 렌더링
@@ -81,8 +122,9 @@ int main(int argc, char* argv[])
 
 		SDL_RenderPresent(renderer);	// 화면에 출력한다.
 
-		// 모든 키를 초기화 한다.
+		// 이전 키를 모두 초기화한다.
 		PrevKeysPressed = {};
+		PreMousePressed = {};
 	}
 
 	SDL_DestroyRenderer(renderer);
@@ -105,7 +147,6 @@ bool GetKeyDown(const SDL_Scancode virtualCode)
 bool GetKeyUp(const SDL_Scancode virtualCode)
 {
 	return not KeysPressed[virtualCode] and PrevKeysPressed[virtualCode];
-
 }
 
 void SetKeyState(const SDL_Scancode virtualCode, const bool bPressed)
@@ -114,3 +155,36 @@ void SetKeyState(const SDL_Scancode virtualCode, const bool bPressed)
 	KeysPressed[virtualCode] = bPressed;
 }
 
+bool GetMouseButton(const Uint8 virtualMouse)
+{
+	return MousePressed[Uint8(virtualMouse)];
+}
+
+bool GetMouseButtonDown(const Uint8 virtualMouse)
+{
+	Uint8 index = Uint8(virtualMouse);
+	return MousePressed[index] and PrevKeysPressed[index];
+}
+
+bool GetMouseButtonUp(const Uint8 virtualMouse)
+{
+	Uint8 index = Uint8(virtualMouse);
+	return not MousePressed[index] and PrevKeysPressed[index];
+}
+
+void SetMouseButtonState(const Uint8 virtualMouse, const bool bPressed)
+{
+	Uint8 index = Uint8(virtualMouse);
+	PrevKeysPressed[index] = (MousePressed[index] != bPressed);
+	MousePressed[index] = bPressed;
+}
+
+Point_2S GetMousePosition()
+{
+	return MousePosition;
+}
+
+void SetMousePosition(Point_2S mousePosition)
+{
+	MousePosition = mousePosition;
+}
