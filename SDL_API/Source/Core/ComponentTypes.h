d1351631a3b0741e90eae00a7e92d57f231157cd
Lee JungSun <dlwjdtjs99@naver.com>
2026-03-17 17:04:42 +0900

    컴포넌트 추가

--- SDL_API/Source/Core/ComponentTypes.h
+++ SDL_API/Source/Core/ComponentTypes.h
@@ -1,1 +1,23 @@
 #pragma once
+#include "Component.h"
+#include "Texture.h"
+
+struct Transfrom final : public Component
+{
+	static constexpr unsigned int id = 0;
+	Transfrom() : Component(&id) {}
+
+	float x;
+	float y;
+	float z;
+	float scale;
+	float angle;
+};
+
+struct Material final : public Component
+{
+	static constexpr unsigned int id = 0;
+	Material() : Component(&id) {}
+
+	Texture* texture;
+};
