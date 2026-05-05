432fcda56338cf12b0c8ed350a2c6ba4a615578b
hiimsw <entryswlee@gmail.com>
2026-05-05 16:20:57 +0900

    충돌 처리 업데이트.

--- SDL_API/Source/Game/StudyScene.cpp
+++ SDL_API/Source/Game/StudyScene.cpp
@@ -2,6 +2,17 @@
 #include "StudyScene.h"
 
 #include "Core/Input.h"
+#include "Core/Collision.h"
+
+enum class CollisionShape
+{
+	None,
+	Box,
+	Line,
+	Circle,
+	Point,
+	Count
+};
 
 // Game
 enum class CollisionLayer
@@ -35,17 +46,32 @@ void StudyScene::Initialize()
 		GetEntityWorld()->AddEntity(&mMainCamera);
 	}
 
+	mBoxTexture.Initialize(GetHelper(), "Resource/box.png");
+
 	for (Entity& mob : mMobs)
 	{
 		Transform transform{};
+
 		Collider collider(static_cast<uint32_t>(CollisionLayer::Monster));
-		collider.CollisionLayerMask.set(uint32_t(CollisionLayer::Player));
-		collider.CollisionLayerMask.set(uint32_t(CollisionLayer::Wall));
+		collider.CollisionLayerMask.set(uint32_t(CollisionLayer::Monster));
+
+		Image image{};
+		image.texture = &mBoxTexture;
+		image.active = true;
+
+		Color color{};
 
 		mob.AddComponent(transform);
 		mob.AddComponent(collider);
+		mob.AddComponent(image);
+		mob.AddComponent(color);
 
 		GetEntityWorld()->AddEntity(&mob);
+	}
+
+	{
+		Transform* transform = mMobs[1].GetComponent<Transform>();
+		transform->position.x += 120.0f;
 	}
 }
 
@@ -91,17 +117,29 @@ bool StudyScene::Update(const float deltaTime)
 					continue;
 				}
 
-				if (1 /* 충돌했나? */)
+				constexpr float BOX_HALF_SIZE = 16.0f;
+
+				Transform* transform0 = entity0->GetComponent<Transform>();
+				Rect rect0 =
 				{
-					std::pair<const Entity*, const Entity*> collidedEntityPair{};
-					if (entity0 < entity1)
-					{
-						collidedEntityPair = { entity0, entity1 };
-					}
-					else
-					{
-						collidedEntityPair = { entity1, entity0 };
-					}
+					.left = transform0->position.x - BOX_HALF_SIZE,
+					.top = transform0->position.y + BOX_HALF_SIZE,
+					.right = transform0->position.x + BOX_HALF_SIZE,
+					.bottom = transform0->position.y - BOX_HALF_SIZE,
+				};
+
+				Transform* transform1 = entity1->GetComponent<Transform>();
+				Rect rect1 =
+				{
+					.left = transform1->position.x - BOX_HALF_SIZE,
+					.top = transform1->position.y + BOX_HALF_SIZE,
+					.right = transform1->position.x + BOX_HALF_SIZE,
+					.bottom = transform1->position.y - BOX_HALF_SIZE,
+				};
+
+				if (Collision::IsCollidedSqureWithSqure(rect0, rect1))
+				{
+					std::pair<const Entity*, const Entity*> collidedEntityPair = getCollidedEntityPair(*entity0, *entity1);
 
 					if (const auto& foundCollidedEntityPair = std::find(mCollidedEntityPairs.begin(), mCollidedEntityPairs.end(), collidedEntityPair);
 						foundCollidedEntityPair == mCollidedEntityPairs.end())
@@ -111,29 +149,33 @@ bool StudyScene::Update(const float deltaTime)
 				}
 			}
 		}
+	}
 
-		for (const auto& collidedEntityPair : mCollidedEntityPairs)
+	// logic
+	{
 		{
-			if (const auto& foundCollidedEntityPair = std::find(mPreviousCollidedEntityPairs.begin(), mPreviousCollidedEntityPairs.end(), collidedEntityPair);
-				foundCollidedEntityPair == mPreviousCollidedEntityPairs.end())
-			{
-				// enter
-			}
-			else
-			{
-				// stay
-			}
+			const float moveX = float(Input::Get().GetKey(SDL_SCANCODE_D) - Input::Get().GetKey(SDL_SCANCODE_A));
+
+			Transform* transform = mMobs[0].GetComponent<Transform>();
+			transform->position.x += moveX * 120.0f * deltaTime;
 		}
 
-		for (const auto& collidedEntityPair : mPreviousCollidedEntityPairs)
+		if (Input::Get().GetKeyDown(SDL_SCANCODE_SPACE))
 		{
-			if (const auto& foundCollidedEntityPair = std::find(mCollidedEntityPairs.begin(), mCollidedEntityPairs.end(), collidedEntityPair);
-				foundCollidedEntityPair == mCollidedEntityPairs.end())
-			{
-				// exit
-			}
+			system("cls");
 		}
 
+		if (isCollisionEnter(mMobs[0], mMobs[1]))
+		{
+			printf("충돌 enter\n");
+		}
+		else if (isCollisionExit(mMobs[0], mMobs[1]))
+		{
+			printf("충돌 exit\n");
+		}
+	}
+
+	{
 		mPreviousCollidedEntityPairs = mCollidedEntityPairs;
 		mCollidedEntityPairs.clear();
 	}
@@ -143,4 +185,57 @@ bool StudyScene::Update(const float deltaTime)
 
 void StudyScene::Finalize()
 {
+	mBoxTexture.Finalize();
 }
+
+std::pair<const Entity*, const Entity*> StudyScene::getCollidedEntityPair(const Entity& entity0, const Entity& entity1) const
+{
+	std::pair<const Entity*, const Entity*> collidedEntityPair{};
+	if (&entity0 < &entity1)
+	{
+		collidedEntityPair = { &entity0, &entity1 };
+	}
+	else
+	{
+		collidedEntityPair = { &entity1, &entity0 };
+	}
+
+	return collidedEntityPair;
+}
+
+bool StudyScene::isCollisionEnter(const Entity& entity0, const Entity& entity1) const
+{
+	std::pair<const Entity*, const Entity*> collidedEntityPair = getCollidedEntityPair(entity0, entity1);
+
+	if (const auto& foundCollidedEntityPair = std::find(mCollidedEntityPairs.begin(), mCollidedEntityPairs.end(), collidedEntityPair);
+		foundCollidedEntityPair != mCollidedEntityPairs.end())
+	{
+		const auto& foundPreviousCollidedEntityPair = std::find(mPreviousCollidedEntityPairs.begin(), mPreviousCollidedEntityPairs.end(), collidedEntityPair);
+		return foundPreviousCollidedEntityPair == mPreviousCollidedEntityPairs.end();
+	}
+
+	return false;
+}
+
+bool StudyScene::isCollisionStay(const Entity& entity0, const Entity& entity1) const
+{
+	std::pair<const Entity*, const Entity*> collidedEntityPair = getCollidedEntityPair(entity0, entity1);
+
+	const auto& foundCollidedEntityPair = std::find(mCollidedEntityPairs.begin(), mCollidedEntityPairs.end(), collidedEntityPair);
+	return foundCollidedEntityPair != mCollidedEntityPairs.end();
+}
+
+bool StudyScene::isCollisionExit(const Entity& entity0, const Entity& entity1) const
+{
+	std::pair<const Entity*, const Entity*> collidedEntityPair = getCollidedEntityPair(entity0, entity1);
+
+	if (const auto& foundPreviousCollidedEntityPair = std::find(mPreviousCollidedEntityPairs.begin(), mPreviousCollidedEntityPairs.end(), collidedEntityPair);
+		foundPreviousCollidedEntityPair != mPreviousCollidedEntityPairs.end())
+	{
+		const auto& foundCollidedEntityPair = std::find(mCollidedEntityPairs.begin(), mCollidedEntityPairs.end(), collidedEntityPair);
+		return foundCollidedEntityPair == mCollidedEntityPairs.end();
+	}
+
+	return false;
+}
+
--- SDL_API/Source/Game/StudyScene.h
+++ SDL_API/Source/Game/StudyScene.h
@@ -2,6 +2,7 @@
 
 #include "Core/ComponentTypes.h"
 #include "Core/Scene.h"
+#include "Core/Texture.h"
 
 #include "Core/Entity/Entity.h"
 
@@ -18,8 +19,16 @@ public:
 	void Finalize()  override;
 
 private:
+	std::pair<const Entity*, const Entity*> getCollidedEntityPair(const Entity& entity0, const Entity& entity1) const;
+	bool isCollisionEnter(const Entity& entity0, const Entity& entity1) const;
+	bool isCollisionStay(const Entity& entity0, const Entity& entity1) const;
+	bool isCollisionExit(const Entity& entity0, const Entity& entity1) const;
+
+private:
+	Texture mBoxTexture{};
+
 	Entity mMainCamera{};
-	Entity mMobs[128]{};
+	Entity mMobs[2]{};
 	std::vector<std::pair<const Entity*, const Entity*>> mCollidedEntityPairs;
 	std::vector<std::pair<const Entity*, const Entity*>> mPreviousCollidedEntityPairs;
 };
